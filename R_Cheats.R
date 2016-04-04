81b945309a0305ced3adb4b4469a71c6dce675fd
anthonyjp87 <john.a.perez@gmail.com>
2016-04-04 21:55:28 +0100

    New cheats

--- R_Cheats.R
+++ R_Cheats.R
@@ -1,3 +1,12 @@
 #Normal Curve over Histogram 
 hist(*data, breaks = 30, freq=FALSE)
 curve(dnorm(x, mean = *data.mean, sd = *data.sd), add = TRUE)
+
+
+#Plotting Residuals 
+resid <- *fit1$residuals
+sd.resid <- sd(resid)
+plot (*data, resid, ylab="Residuals", pch=20)
+abline (sd.resid,0,lty=2)
+abline(0,0)
+abline (-sd.resid,0,lty=2)
