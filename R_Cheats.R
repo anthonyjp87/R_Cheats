0885a96533a23299bbbd9707d14a72487b3454c5
anthonyjp87 <john.a.perez@gmail.com>
2016-04-14 20:24:52 +0100

    Replace Unique w/int

--- R_Cheats.R
+++ R_Cheats.R
@@ -18,30 +18,37 @@ abline (-sd.resid,0,lty=2)
 
 #Removing Column from Data Table: 
 tablename[,columnname:=NULL]
+tablename[,c('columnname','colname2'):=NULL]
 
-#Function to split 168 Hour Week into Day/Night 1/0
-day_night <- function(test_hr,s,e){
+#Function to split 168 Hour Week into Day/Night 1/0. b1, b2 are the breaks between start/end
+day_night <- function(test_hr,b1,b2){
+  
   test_hr<-ifelse(test_hr==1,1.1,test_hr)
   for(i in 0:6){
-    test_hr<-ifelse(test_hr> (s+24*i) & test_hr< (e+24*i), 1 ,test_hr)
+    test_hr<-ifelse(test_hr> (b1+24*i) & test_hr< (b2+24*i), 1 ,test_hr)
   }
   test_hr<-ifelse(test_hr==1,1,0)
   return(test_hr)
 }
 
+
 #Function to divide 168 Wrk Week into 3 sections per day
-#b's are breaks:
-Multi_divide <- function(test_hr,b1,b2,b3){
+#b1, b2, b3's are breaks in a day
+multi_divide <- function(test_hr,b1,b2,b3){
   test_hr<-ifelse(test_hr==1,1.1,test_hr)
   test_hr<-ifelse(test_hr==2,2.1,test_hr)
-  
   for(i in 0:6){
     test_hr<-ifelse(test_hr> (b1+24*i) & test_hr<= (b2+24*i), 1 ,test_hr)
     test_hr<-ifelse(test_hr> (b2+24*i) & test_hr<= (b3+24*i), 2 ,test_hr)
   }
   test_hr<-ifelse(test_hr!=1 & test_hr!=2,0,test_hr)
-  print(test_hr)
   return(test_hr)
 }
+
+#Replace all unique values with Ints
+for(i in 1:length(unique(col))){
+  dt[col==unique(col[i]),col:=as.character(i)]
+}
+
 
 
